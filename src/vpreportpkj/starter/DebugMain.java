4bc6783699c31c3204e696bfc7866f8034533a26
AlexIOscar <zobr85@list.ru>
2022-11-03 15:11:36 +0600

    1. Добавлен дебаг-код в DebugMain

--- src/vpreportpkj/starter/DebugMain.java
+++ src/vpreportpkj/starter/DebugMain.java
@@ -1,7 +1,24 @@
 package vpreportpkj.starter;
 
-public class DebugMain {
-    public static void main(String[] args) {
+import vpreportpkj.core.LabourEngine;
+import vpreportpkj.core.SingleTuple;
+import vpreportpkj.core.Util;
+import vpreportpkj.core.labrepo.AdvancedRepo;
 
+import java.io.IOException;
+import java.util.List;
+
+public class DebugMain {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        LabourEngine instance = LabourEngine.getInstance("C:\\Users\\user\\VPRP\\pcRepo.dat");
+        List<SingleTuple> commonList = Util.getCommonList("C:\\Users\\user\\Desktop\\тестовый каталог IN");
+
+        AdvancedRepo repo = (AdvancedRepo) instance.getRepository();
+        repo.setUpdate(false);
+        repo.setFilterFactor(2);
+        for (SingleTuple st: commonList
+             ) {
+            System.out.println(repo.chkTime(st));
+        }
     }
 }
